71bd4c24bd131cbb21741441ac917dfd42d5fc37
Nikhil kamthe <124755912+nikhilkamthe07@users.noreply.github.com>
2025-03-01 20:48:04 +0530

    Add files via upload

---
+++ Dot-net-assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String Value");
+            String str = Console.ReadLine();
+            Console.WriteLine(str);
+
+            if (int.TryParse(str, out int intValue))
+            {
+                Console.WriteLine($"Converted to Integer: {intValue}");
+            }
+            else if (bool.TryParse(str, out bool boolValue))
+            {
+                Console.WriteLine($"Converted to Boolean: {boolValue}");
+            }
+            else if (double.TryParse(str, out double doubleValue))
+            {
+                Console.WriteLine($"Converted to Double: {doubleValue}");
+            }
+            else if (decimal.TryParse(str, out decimal decimalValue))
+            {
+                Console.WriteLine($"Converted to Decimal: {decimalValue}");
+            }
+            else if (DateTime.TryParse(str, out DateTime dateTimeValue))
+            {
+                Console.WriteLine($"Converted to DateTime: {dateTimeValue}");
+            }
+            else
+            {
+                Console.WriteLine("Unable to convert the input to a recognized data type.");
+            }
+        }
+    }
+}
---
+++ Question-2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Take input from the user
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            // 1. Convert to uppercase (Example of a string function)
+            string upperCaseString = input.ToUpper();
+            Console.WriteLine("Uppercase: " + upperCaseString);
+
+            // 2. Reverse the string
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedString = new string(charArray);
+            Console.WriteLine("Reversed: " + reversedString);
+
+            // 3. Replace spaces with '-'
+            string replacedString = input.Replace(" ", "-");
+            Console.WriteLine("Replaced Spaces: " + replacedString);
+
+            // 4. Get length of the string
+            int length = input.Length;
+            Console.WriteLine("Length of the string: " + length);
+
+            // Find occurrences of 'l' and their positions
+            int count = 0;
+            Console.Write("Positions of 'l': ");
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'l')
+                {
+                    count++;
+                    Console.Write(i + " "); // Print position of 'l'
+                }
+            }
+            Console.WriteLine("\nNumber of times 'l' appears: " + count);
+
+            // Formatting output as *H*e*l*l*o*
+            string formattedOutput = "*" + string.Join("*", input.ToCharArray()) + "*";
+            Console.WriteLine("Formatted Output: " + formattedOutput);
+        }
+    }
+}
