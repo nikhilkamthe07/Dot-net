058c81ba4d8fb00e78672548825db2ce9cd0f468
Nikhil kamthe <124755912+nikhilkamthe07@users.noreply.github.com>
2025-03-01 20:48:42 +0530

    Add files via upload

---
+++ Question-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "output.txt"; // File to store data
+
+            // Taking user input
+            Console.Write("Enter a string to append to the file: ");
+            string userInput = Console.ReadLine();
+
+            // Appending text to the file
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(userInput);
+            }
+
+            Console.WriteLine("Text appended successfully!");
+        }
+    }
+}
